aa79389f14f2de1668deac7214cca786dc5170ec
Elsever-Emirli <elseveremirli10@gmail.com>
2024-03-25 23:02:29 +0400

    Norhwind Front End

---
+++ src/layouts/SignedIn.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Dropdown, Menu,Image } from 'semantic-ui-react'
+
+export default function SignedIn({signOut}) {
+    return (
+        <div>
+            <Menu.Item>
+                <Image avatar spaced="right" src=""/>
+                <Dropdown pointing="top left" text="Elsever">
+                    <Dropdown.Menu>
+                        <Dropdown.Item text="Bilgilerim" icon="info"/>
+                        <Dropdown.Item onClick={signOut} text="Çıkış Yap" icon="sign-out"/>
+                    </Dropdown.Menu>
+                </Dropdown>
+            </Menu.Item>
+        </div>
+    )
+}
+
+//22.50 Dersteyiz
